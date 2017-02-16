c36f73ba09f2b0c3746fe15453e49503dd40fd4f
francescobianco <info.francescobianco@gmail.com>
2017-02-17 00:49:26 +0100

    next

---
+++ exec/ndev.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd ../..
+mkdir ndev_modules > /dev/null 2>&1
+mkdir node_modules > /dev/null 2>&1
+rm -fr ndev_modules/$2
+rm -fr node_modules/$2
+cd node_modules
+git clone $1 $2
+cd ..
+ln -s ../node_modules/$2 ndev_modules/$2
+
+
+
--- src/ndev.js
+++ src/ndev.js
@@ -1,8 +1,19 @@
 
+var exec = require("child_process").exec;
+
 module.exports = {
 
-    run: function() {
-        console.log("Ciao")
+    run: function(args) {
+
+        var repo = args[0];
+
+        var name = args[1];
+
+        exec("./exec/ndev.sh " + repo + " " + name,
+            function (error, stdout, stderr) {
+                console.log(error, stdout, stderr);
+            }
+        );
     },
 
     sayHelloInSpanish: function() {
