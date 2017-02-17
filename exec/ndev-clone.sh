5936a23ce113e4c228966700c151571299331b15
francescobianco <info.francescobianco@gmail.com>
2017-02-17 11:55:15 +0100

    version 0.0.4

--- exec/ndev-clone.sh
+++ exec/ndev-clone.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-cd ..
+cd $1
 mkdir ndev_modules > /dev/null 2>&1
 mkdir node_modules > /dev/null 2>&1
-rm -fr ndev_modules/$2
-rm -fr node_modules/$2
+rm -fr ndev_modules/$3
+rm -fr node_modules/$3
 cd node_modules
-git clone $1 $2
+git clone $2 $3
 cd ..
-ln -s ../node_modules/$2 ndev_modules/$2
+ln -s ../node_modules/$3 ndev_modules/$3
 
 
 
--- src/ndev.js
+++ src/ndev.js
@@ -1,21 +1,38 @@
 
+//
 var exec = require("child_process").exec;
 
+//
 module.exports = {
 
+    //
     run: function(args) {
 
-        var repo = args[0];
+        //
+        var tool = args[0];
 
-        var name = args[1];
-
-        exec(__dirname + "/../exec/ndev.sh " + repo + " " + name,
-            function (error, stdout, stderr) {
-                console.log(stderr.trim());
-            }
-        );
+        //
+        switch (tool) {
+            case "clone": return ndev_clone(args);
+        }
     }
 };
+
+//
+function ndev_clone(args) {
+
+    var path = process.cwd();
+
+    var repo = args[1];
+
+    var name = args[2];
+
+    exec(__dirname + "/../exec/ndev-clone.sh " + path + " " + repo + " " + name,
+        function (error, stdout, stderr) {
+            console.log(stderr);
+        }
+    );
+}
 
 
 
