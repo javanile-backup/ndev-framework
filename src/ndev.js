c61fdf117ddf4d12b126bc3efcd95838a728813e
francescobianco <info.francescobianco@gmail.com>
2017-03-27 00:56:21 +0200

    version 0.0.16

--- src/ndev.js
+++ src/ndev.js
@@ -3,49 +3,52 @@
  *
  */
 
-//
-var exec     = require("child_process").exec;
-var basename = require("path").basename;
-
-//
 var path = process.cwd();
+var base = require("path").basename;
+var exec = require("child_process").exec;
 
-//
 module.exports = {
-
-    //
+    /**
+     *
+     *
+     * @param args
+     */
     run: function(args) {
-
-        //
-        var tool = args[0];
-
-        //
-        switch (tool) {
-            case "clone": return ndev_clone(args);
-            case "install": return ndev_install(args);
-            case "publish": return ndev_publish(args);
-            case "project": return ndev_project(args);
-            default: console.error("Undefined tool:", tool);
+        switch (args[0]) {
+            case "clone": ndev_clone(args); break;
+            case "install": ndev_install(args); break;
+            case "publish": ndev_publish(args); break;
+            case "project": ndev_project(args); break;
+            default: console.error("(ndev) Undefined command:", args[0]);
         }
     }
-}
+};
 
-//
+/**
+ *
+ *
+ * @param args
+ */
 function ndev_clone(args) {
-
-    //
     if (!args[1]) {
-        console.error("Error repository required.");
+        console.error("(ndev) Required repository url or package name.");
         return;
     }
 
     //
+    if (!args[1].match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)) {
+        var pack = args[1];
+        var name = args[2] ? args[2] : base(repo, ".git");
+        exec(__dirname + "/../exec/ndev-clone-pack.sh " + path + " " + repo + " " + name,
+            function (error, stdout, stderr) {
+                console.log(stderr.trim());
+            }
+        );
+    }
+
+    //
     var repo = args[1];
-
-    //
     var name = args[2] ? args[2] : basename(repo, ".git");
-
-    //
     exec(__dirname + "/../exec/ndev-clone.sh " + path + " " + repo + " " + name,
         function (error, stdout, stderr) {
             console.log(stderr.trim());
@@ -53,13 +56,15 @@ function ndev_clone(args) {
     );
 }
 
-//
+/**
+ *
+ * @param args
+ */
 function ndev_install(args) {
+    if (!args[1]) { return e("Required package name."); }
 
     //
-    console.log("Please wait during install...");
-
-    //
+    console.log("(ndev) Please wait during install...");
     exec(__dirname + "/../exec/ndev-install.sh " + path + " " + args.slice(1).join(" "),
         function (error, stdout, stderr) {
             console.log(!stderr ? stdout.trim() : stderr.trim());
@@ -67,22 +72,15 @@ function ndev_install(args) {
     );
 }
 
-//
+/**
+ *
+ * @param args
+ */
 function ndev_publish(args) {
+    if (!args[1]) { return e("Required ndev module to publish."); }
 
-    //
-    if (!args[1]) {
-        console.error("Error ndev module required.");
-        return;
-    }
-
-    //
+    i("Please wait during publish...");
     var name = args[1];
-
-    //
-    console.log("Please wait during publish...");
-
-    //
     exec(__dirname + "/../exec/ndev-publish.sh " + path + " " + name,
         function (error, stdout, stderr) {
             console.log(stderr.trim(), stdout);
@@ -92,18 +90,13 @@ function ndev_publish(args) {
 
 //
 function ndev_project(args) {
-
-    //
-    if (!args[1]) {
-        console.error("Error repository required.");
-        return;
-    }
+    if (!args[1]) { return e("Error repository required."); }
 
     //
     var repo = args[1];
 
     //
-    var name = args[2] ? args[2] : basename(repo, ".git");
+    var name = args[2] ? args[2] : base(repo, ".git");
 
     //
     console.log("Please wait during project creation...");
@@ -116,7 +109,11 @@ function ndev_project(args) {
     );
 }
 
-
+/**
+ *
+ */
+function i (msg) { console.log("(ndev)", msg); }
+function e (msg) { console.error("(ndev)", msg); }
 
 
 
