6234f779f1b476cdf37cc078c7405361c6ba5b7e
francescobianco <info.francescobianco@gmail.com>
2017-04-20 19:47:38 +0200

    version 0.0.16

--- test/test.js
+++ test/test.js
@@ -2,16 +2,17 @@
 
 var ndev = require("../src/ndev");
 var util = require("../src/util");
-var assert = require("chai").assert;
+var chai = require('chai');
+
+chai.use(require('chai-fs'));
 
 describe("first test", function () {
     it("should copy the title", function (done) {
-
         ndev.cwd = __dirname + "/tmp/clone/";
         ndev.cmdClone(["https://github.com/javanile/declinejs"], function(out) {
-            console.log(out);
+            chai.assert.isDirectory(ndev.cwd + "/ndev_modules/declinejs/");
+            chai.assert.isFile(ndev.cwd + "/ndev_modules/declinejs/LICENSE");
             done();
         });
-
     });
 });
