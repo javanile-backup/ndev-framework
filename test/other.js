13f47ebf048b0e374cbe329f781916512132ace1
francescobianco <info.francescobianco@gmail.com>
2017-04-21 17:01:10 +0200

    version 0.0.16

--- test/other.js
+++ test/other.js
@@ -1,10 +1,13 @@
 "use strict";
 
 var util = require("../src/util");
+var path = require("path");
 var chai = require("chai");
 
+chai.use(require("chai-fs"));
+
 describe("Other tests", function () {
-    it("Simple test", function () {
-        chai.assert.equal(util.pad(2), "  ");
+    it("Have bin file ndev", function () {
+        chai.assert.isFile(path.join(__dirname, "../bin/ndev"));
     });
 });
