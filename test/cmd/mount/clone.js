74500704f5f6ce7b3abd3dadfaa78af76ddb36d9
francescobianco <info.francescobianco@gmail.com>
2017-04-21 13:57:41 +0200

    version 0.0.16

---
+++ test/cmd/mount/clone.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var ndev = require("../../../src/ndev");
+var util = require("../../../src/util");
+var chai = require("chai");
+
+chai.use(require("chai-fs"));
+
+describe("Testing 'clone' commnad", function () {
+    describe("Clone by repository url", function () {
+        it("Test folders and files exists", function () {
+            ndev.cwd = __dirname + "/../../../tmp/clone/";
+            ndev.cmdClone(["https://github.com/javanile/declinejs"], function(log) {
+                chai.assert.isDirectory(ndev.cwd + "/ndev_modules/declinejs/");
+                chai.assert.isFile(ndev.cwd + "/ndev_modules/declinejs/LICENSE");
+                done();
+            });
+        });
+    });
+});
---
+++ test/cmd/unfreeze/clone.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var ndev = require("../../../src/ndev");
+var util = require("../../../src/util");
+var assert = require("chai").assert;
+
+describe("Testing 'clone' commnad", function () {
+    describe("Clone by repository url", function () {
+        it("should copy the title", function () {
+            assert.equal(util.pad(2), "  ");
+        });
+    });
+});
